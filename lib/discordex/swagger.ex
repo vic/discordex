b4661eb7ad41a6a9b4491515254843dba8ba096c
David Gu <david.gurquiza@gmail.com>
2016-01-25 21:43:55 -0800

    Checkpoint

--- lib/discordex/swagger.ex
+++ lib/discordex/swagger.ex
@@ -1,65 +1,135 @@
 defmodule Discordex.Swagger do
 
-  def parse_yaml(filename \\ "swagger.yaml") do
+  # Specify an external resource to the current module to recompile in case it changes
+  @external_resource "swagger.yaml"
+
+  def parse_yaml(filename \\ @external_resource) do
     File.cwd!
     |> Path.join(filename)
     |> YamlElixir.read_from_file
   end
 
-  # Parameters may be reserved keywords :( so we need to add something to avoid that
-  # ** (SyntaxError) nofile:1: syntax error before: 'after'
-  @param_suffix "_"
-
   require Logger
   require IEx
   defmacro generate_functions do
     swagger = parse_yaml
-    base_url = generate_base_url(swagger)
-    swagger["paths"]
-    |> Enum.map fn {path, verbs} ->
-      verbs
-      |> Enum.map fn {verb, request} ->
-        verb = verb |> String.to_atom
-        function_name = request["operationId"] |> String.to_atom
-        parameters = request["parameters"] |> generate_parameters(swagger)
-        template_path = path
-        |> String.replace("{", ~s(\#\{))
-        |> String.replace("}", ~s(#{@param_suffix}\}))
-        quoted_path = Code.string_to_quoted!(~s("#{template_path}"))
+    quote location: :keep do
+      unquote(defendpoints(swagger))
+    end
+  end
 
-        # Logger.debug("#{verb} #{template_path} #{inspect parameters}")
-        # IO.puts(~s{assert Swag.#{function_name}() == "#{template_path}"})
-        # responses = request["responses"]
-        quote do
-          # @spec parameter_types :: response_types
-          def unquote(function_name)(unquote_splicing(parameters)) do
-            unquote(base_url) <> unquote(quoted_path)
-            # HTTPoison.request(verb, )
-          end
-        end
+  def defendpoints(swagger) do
+    swagger["paths"]
+    |> Enum.map(fn {path, verbs} ->
+      verbs
+      |> Enum.map(fn {verb, config} ->
+        function_name = config["operationId"] |> String.to_atom
+        verb = verb |> String.to_atom
+        swagger_params = config["parameters"] |> expand_params(swagger)
+        defendpoint(swagger, function_name, verb, path, swagger_params)
+
+        # Logger.debug("#{function_name}: #{verb} #{template_path} #{inspect params}")
+      end)
+    end)
+  end
+
+  def defendpoint(swagger, function_name, verb, path, swagger_params) do
+    quote location: :keep do
+      # @spec parameter_types :: response_types
+
+      def unquote(function_name)(client, raw_params) do
+        params = raw_params
+        # |> Discordex.Swagger.check_required_params!(unquote(swagger_params))
+        # |> Discordex.Swagger.set_default_params(unquote(swagger_params))
+
+        # Set request parameteres
+        # case param["in"] do
+        #   "query" ->
+        #     # /items?id=###
+        #     nil
+        #   "header" ->
+        #     client
+        #     nil
+        #   "body" ->
+        #     nil
+        #   "form" ->
+        #     nil
+        # end
+
+        # Render path
+        rendered_path = Discordex.Swagger.render_path(unquote(path), params)
+
+        # Generate full url
+        full_url = unquote(base_url(swagger)) <> rendered_path
+
+        # {unquote(verb), url}
+
+        # unquote_splicing(params)
+        # unquote(base_url) <> unquote(quoted_path)
+        # HTTPoison.request(verb, )
       end
     end
   end
 
-  def generate_base_url(swagger) do
+  def check_required_params!(params, swagger_params) do
+    swagger_params
+    |> Enum.reduce(params, fn(swagger_param, params) ->
+      param_key = String.to_atom(swagger_param["name"])
+      # Enforce required parameter
+      if swagger_param["required"] do
+        # TODO: Raise all missing parameters, not only one
+        unless Keyword.has_key?(params, param_key) do
+          raise "missing #{param_key}"
+        end
+      end
+    end)
+  end
+
+  def set_default_params(params, swagger_params) do
+    swagger_params
+    |> Enum.reduce(params, fn(swagger_param, params) ->
+      param_key = String.to_atom(swagger_param["name"])
+      if swagger_param["default"] do
+        Keyword.update(params, param_key, swagger_param["default"], &(&1))
+      end
+    end)
+  end
+
+  # # A path may contain template segments like /users/{user-id}/ so we replace those
+  def render_path(path, path_params) do
+    ~r/(?<head>){[^\}]+}(?<tail>)/
+    |> Regex.split(path, on: [:head, :tail])
+    |> Enum.reduce("", fn
+      "{" <> path_param, acc ->
+        key = path_param
+        |> String.rstrip(?\})
+        |> String.to_atom
+        # Render segment with binding from `params`
+        acc <> to_string(Dict.fetch!(path_params, key))
+
+      segment, acc ->
+        # Append segment to the path
+        acc <> segment
+    end)
+  end
+
+  def base_url(swagger) do
+    # TODO: Support more than 1 scheme
     scheme = swagger["schemes"] |> hd
     scheme <> "://" <> swagger["host"] <> swagger["basePath"]
   end
 
-  def generate_parameters(nil, _), do: []
-  def generate_parameters(parameters, swagger) do
-    parameters
-    |> Enum.map(fn parameter ->
-      if(ref = parameter["$ref"]) do
-        "#/parameters/" <> parameter_name = ref
-        parameter = Map.fetch!(swagger["parameters"], parameter_name)
-      end
-      # parameter["in"]
-      # parameter["schema"]
-      # parameter["required"]
-      parameter["name"] <> @param_suffix
-    end)
-    |> Enum.map(&Code.string_to_quoted!/1)
-  end
+  # # A parameter may be a reference like '#/parameter/user-id' so we expand those
+  def expand_params(nil, _), do: []
+  def expand_params(params, swagger) do
+    for param <- params do
+      if(ref = param["$ref"]) do
+        "#/parameters/" <> param_name = ref
 
+        # Replace with root definition
+        param = Map.fetch!(swagger["parameters"], param_name)
+      end
+      param
+    end
+  end
 end
--- test/discordex/swagger_test.exs
+++ test/discordex/swagger_test.exs
@@ -7,7 +7,14 @@ defmodule Discordex.SwaggerTest do
   end
 
   test "swagger" do
-    assert Swag.get_user("123abc") == "https://discordapp.com/api/users/123abc"
+    assert Swag.get_user("1", user_id: "123abc") == "https://discordapp.com/api/users/123abc"
+  end
+
+  test "missing parameter" do
+    # TODO: Create proper Error
+    assert_raise RuntimeError, fn ->
+      Swag.get_user("1", username: "abc")
+    end
   end
 
 end
